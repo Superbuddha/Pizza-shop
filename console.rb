160c727ebf31f0a72ccab8e4ed41910a8857d0a6
Stephan Constantial <Sconstantial@yahoo.com>
2018-09-14 10:36:02 +0100

    pizza shop completed

--- console.rb
+++ console.rb
@@ -5,5 +5,7 @@ order1 = PizzaOrder.new({ 'first_name'=>'Luke', 'last_name'=> 'Skywalker', 'quan
 order2 = PizzaOrder.new({ 'first_name'=>'Darth', 'last_name'=> 'Vader', 'quantity'=> '1', 'topping'=> 'Quattro Formaggi'})
 
 order1.save()
+
+order1.first_name = "Fred"
 binding.pry
 nil
